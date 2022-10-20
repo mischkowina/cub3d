36580a61f8bf88529f11b70c1c8ccf8b38c95398
Saskia Mischnick <smischni@student.42wolfsburg.de>
2022-10-20 14:13:39 +0200

    Test

---
+++ cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 11:51:04 by smischni          #+#    #+#             */
+/*   Updated: 2022/10/20 11:56:08 by smischni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "/usr/X11/include/X11/X.h"
+# include <math.h>
+
+#endif
